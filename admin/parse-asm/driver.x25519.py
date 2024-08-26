a9797c75313aecb584d6379144d97aac073775e3
Joe Birr-Pixton <jpixton@gmail.com>
2024-08-26 11:26:55 +0100

    driver.x25519.py: fix broken paths

--- admin/parse-asm/driver.x25519.py
+++ admin/parse-asm/driver.x25519.py
@@ -6,8 +6,10 @@ from driver import (
 )
 
 if __name__ == "__main__":
-    with open("../../thirdparty/s2n-bignum/x../..//curve25519_x25519.S") as input, open(
-        "../../src/low/x86_../../_x25519.rs", "w"
+    with open(
+        "../../thirdparty/s2n-bignum/x86/curve25519/curve25519_x25519.S"
+    ) as input, open(
+        "../../graviola/src/low/x86_64/curve25519_x25519.rs", "w"
     ) as output:
         d = RustDriver(output, Architecture_amd64)
         d.emit_rust_function(
@@ -22,8 +24,10 @@ if __name__ == "__main__":
         parse_file(input, d)
 
     with open(
-        "../../thirdparty/s2n-bignum/x../..//curve25519_x25519base.S"
-    ) as input, open("../../src/low/x86_../../_x25519base.rs", "w") as output:
+        "../../thirdparty/s2n-bignum/x86/curve25519/curve25519_x25519base.S"
+    ) as input, open(
+        "../../graviola/src/low/x86_64/curve25519_x25519base.rs", "w"
+    ) as output:
         d = RustDriver(output, Architecture_amd64)
         d.add_const_symbol("curve25519_x25519base_edwards25519_0g")
         d.add_const_symbol("curve25519_x25519base_edwards25519_8g")
@@ -38,8 +42,10 @@ if __name__ == "__main__":
         )
         parse_file(input, d)
 
-    with open("../../thirdparty/s2n-bignum/a../..//curve25519_x25519.S") as input, open(
-        "../../src/low/aarch../../_x25519.rs", "w"
+    with open(
+        "../../thirdparty/s2n-bignum/arm/curve25519/curve25519_x25519.S"
+    ) as input, open(
+        "../../graviola/src/low/aarch64/curve25519_x25519.rs", "w"
     ) as output:
         d = RustDriver(output, Architecture_aarch64)
         d.emit_rust_function(
@@ -54,8 +60,10 @@ if __name__ == "__main__":
         parse_file(input, d)
 
     with open(
-        "../../thirdparty/s2n-bignum/a../..//curve25519_x25519base.S"
-    ) as input, open("../../src/low/aarch../../_x25519base.rs", "w") as output:
+        "../../thirdparty/s2n-bignum/arm/curve25519/curve25519_x25519base.S"
+    ) as input, open(
+        "../../graviola/src/low/aarch64/curve25519_x25519base.rs", "w"
+    ) as output:
         d = RustDriver(output, Architecture_aarch64)
         d.add_const_symbol("curve25519_x25519base_edwards25519_0g")
         d.add_const_symbol("curve25519_x25519base_edwards25519_8g")
