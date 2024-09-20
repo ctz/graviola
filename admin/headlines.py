eaf24d211e2160f5537255d0872ed1b98bb3d9f0
Joe Birr-Pixton <jpixton@gmail.com>
2024-09-20 11:38:17 +0100

    Brighten things up

--- admin/headlines.py
+++ admin/headlines.py
@@ -9,23 +9,27 @@ impls = "aws-lc-rs dalek ring graviola rustcrypto".split()
 which = {
     ("rsa2048-pkcs1-sha256-verify",): {
         "name": "RSA2048 signature verification",
-        "format": lambda v: "{:,g} sigs/sec".format(v),
+        "format": lambda v: "<data value='{0}'>{0:,.5g}</data> sigs/sec".format(v),
     },
     ("rsa2048-pkcs1-sha256-sign",): {
         "name": "RSA2048 signing",
-        "format": lambda v: "{:,g} sigs/sec".format(v),
+        "format": lambda v: "<data value='{0}'>{0:,.5g}</data> sigs/sec".format(v),
     },
     ("x25519-ecdh",): {
         "name": "X25519 key agreement",
-        "format": lambda v: "{:,g} kx/sec".format(v),
+        "format": lambda v: "<data value='{0}'>{0:,.5g}</data> kx/sec".format(v),
     },
     ("aes256-gcm", "16KB"): {
         "name": "AES256-GCM encryption (16KB wide)",
-        "format": lambda v: "%.03g GiB/sec" % (v * 16384 / (1024 * 1024 * 1024)),
+        "format": lambda v: "<data value='{0}'>{1:,.03g}</data> GiB/sec".format(
+            v, v * 16384 / (1024 * 1024 * 1024)
+        ),
     },
 }
 
 results = []
+
+standings = ["🥇", "🥈", "🥉", " ", " "]
 
 for arch in sorted(archs):
     for wkey, wdesc in sorted(which.items()):
@@ -56,20 +60,29 @@ for desc, arch, impl, value in results:
     time = value * 1e-9
     rate = 1 / time
     value = desc["format"](rate)
-    tree.setdefault(arch, {}).setdefault(desc["name"], {})[impl] = value
+    tree.setdefault(arch, {}).setdefault(desc["name"], {})[impl] = (value, rate)
 
 fragment = StringIO()
 for arch, descs in sorted(tree.items()):
     print("<h2>{}</h2>".format(arch), file=fragment)
     for desc, impls in sorted(descs.items()):
-        print("<h3>{}</h3><table width='80%'><tr>".format(desc), file=fragment)
+        print(
+            "<h3>{}</h3><table width='80%' cellspacing=25 cellpadding=10><tr>".format(
+                desc
+            ),
+            file=fragment,
+        )
 
-        for impl in sorted(impls.keys()):
-            print("<th align='left'>{}</th>".format(impl), file=fragment)
-        print("</tr><tr>", file=fragment)
+        for standing, impl in zip(
+            standings, sorted(impls.keys(), key=lambda b: impls[b][1], reverse=True)
+        ):
+            print(
+                "<td class='{}'><h3>{} {}</h3>{}</td>".format(
+                    impl, standing, impl, impls[impl][0]
+                ),
+                file=fragment,
+            )
 
-        for impl in sorted(impls.keys()):
-            print("<td><big>{}</big></td>".format(impls[impl]), file=fragment)
         print("</tr></table>", file=fragment)
 
 
