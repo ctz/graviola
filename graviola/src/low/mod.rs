86870b900df6d2420acd0df39a060cd79cef6fa3
Joe Birr-Pixton <jpixton@gmail.com>
2024-09-27 20:34:39 +0100

    low: address dead_code

--- graviola/src/low/mod.rs
+++ graviola/src/low/mod.rs
@@ -1,9 +1,25 @@
 // Written for Graviola by Joe Birr-Pixton, 2024.
 // SPDX-License-Identifier: Apache-2.0 OR ISC OR MIT-0
 
-mod generic;
 #[macro_use]
 mod macros;
+
+mod generic {
+    #[cfg(target_arch = "aarch64")]
+    pub(crate) mod aes_gcm;
+    pub(super) mod blockwise;
+    #[cfg(target_arch = "aarch64")]
+    pub(crate) mod chacha20;
+    pub(super) mod ct_equal;
+    #[cfg(test)]
+    pub(crate) mod ghash;
+    #[cfg(target_arch = "aarch64")]
+    pub(super) mod optimise_barrier;
+    pub(crate) mod poly1305;
+    pub(super) mod sha256;
+    pub(super) mod sha512;
+}
+
 mod posint;
 
 pub(crate) use generic::blockwise::Blockwise;
