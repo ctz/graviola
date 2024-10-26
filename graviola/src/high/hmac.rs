c311b00bf3ab274c17bfc20f461d42755d13fff7
Joe Birr-Pixton <jpixton@gmail.com>
2024-10-26 13:23:54 +0100

    hmac: cavp tests

--- graviola/src/high/hmac.rs
+++ graviola/src/high/hmac.rs
@@ -75,9 +75,10 @@ impl<H: Hash> Hmac<H> {
 }
 
 #[cfg(test)]
-mod manual_tests {
+mod tests {
     use super::*;
-    use crate::high::hash::Sha256;
+    use crate::high::hash::{Sha256, Sha384, Sha512};
+    use crate::test::*;
 
     #[test]
     fn smoke() {
@@ -85,5 +86,107 @@ mod manual_tests {
         h.update(b"world");
         assert_eq!(h.finish(),
                    HashOutput::Sha256(*b"\xf1\xac\x97\x02\xeb\x5f\xaf\x23\xca\x29\x1a\x4d\xc4\x6d\xed\xde\xee\x2a\x78\xcc\xda\xf0\xa4\x12\xbe\xd7\x71\x4c\xff\xfb\x1c\xc4"));
+    }
+
+    #[test]
+    fn cavp() {
+        #[derive(Debug)]
+        enum Kind {
+            None,
+            Sha256,
+            Sha384,
+            Sha512,
+        }
+
+        #[derive(Debug)]
+        struct Cavp {
+            kind: Kind,
+            key: Vec<u8>,
+            message: Vec<u8>,
+        }
+
+        impl Default for Cavp {
+            fn default() -> Self {
+                Self {
+                    kind: Kind::None,
+                    key: Vec::new(),
+                    message: Vec::new(),
+                }
+            }
+        }
+
+        impl CavpSink for Cavp {
+            fn on_meta(&mut self, meta: &str) {
+                self.kind = match meta {
+                    "L=20" | "L=28" => Kind::None,
+                    "L=32" => Kind::Sha256,
+                    "L=48" => Kind::Sha384,
+                    "L=64" => Kind::Sha512,
+                    _ => panic!("unhandled {meta:?}"),
+                };
+            }
+
+            fn on_value(&mut self, name: &str, value: Value<'_>) {
+                match name {
+                    "Klen" | "Tlen" => {}
+                    "Count" => println!("  test {}", value.int()),
+                    "Key" => self.key = value.bytes(),
+                    "Msg" => self.message = value.bytes(),
+                    "Mac" => match self.kind {
+                        Kind::None => {}
+                        Kind::Sha256 => {
+                            let mut h = Hmac::<Sha256>::new(&self.key);
+                            h.update(&self.message);
+                            let tag = h.finish();
+
+                            let wanted = value.bytes();
+
+                            match wanted.len() {
+                                32 => assert!(tag.ct_equal(&wanted)),
+                                24 => assert!(tag.truncated_ct_equal::<24>(&wanted)),
+                                16 => assert!(tag.truncated_ct_equal::<16>(&wanted)),
+                                len => todo!("unhandled tag len {len}"),
+                            }
+                        }
+                        Kind::Sha384 => {
+                            let mut h = Hmac::<Sha384>::new(&self.key);
+                            h.update(&self.message);
+                            let tag = h.finish();
+
+                            let wanted = value.bytes();
+
+                            match wanted.len() {
+                                48 => assert!(tag.ct_equal(&wanted)),
+                                40 => assert!(tag.truncated_ct_equal::<40>(&wanted)),
+                                32 => assert!(tag.truncated_ct_equal::<32>(&wanted)),
+                                24 => assert!(tag.truncated_ct_equal::<24>(&wanted)),
+                                len => todo!("unhandled tag len {len}"),
+                            }
+                        }
+                        Kind::Sha512 => {
+                            let mut h = Hmac::<Sha512>::new(&self.key);
+                            h.update(&self.message);
+                            let tag = h.finish();
+
+                            let wanted = value.bytes();
+
+                            match wanted.len() {
+                                64 => assert!(tag.ct_equal(&wanted)),
+                                56 => assert!(tag.truncated_ct_equal::<56>(&wanted)),
+                                48 => assert!(tag.truncated_ct_equal::<48>(&wanted)),
+                                40 => assert!(tag.truncated_ct_equal::<40>(&wanted)),
+                                32 => assert!(tag.truncated_ct_equal::<32>(&wanted)),
+                                len => todo!("unhandled tag len {len}"),
+                            }
+                        }
+                    },
+                    _ => {
+                        todo!("{self:?} value {name} = {value:?}");
+                    }
+                }
+            }
+        }
+
+        process_cavp("../thirdparty/cavp/hmac/HMAC.rsp", &mut Cavp::default());
     }
 }
---
+++ graviola/src/test.rs
@@ -0,0 +1,57 @@
+// Written for Graviola by Joe Birr-Pixton, 2024.
+// SPDX-License-Identifier: Apache-2.0 OR ISC OR MIT-0
+
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::Path;
+
+pub(crate) trait CavpSink {
+    fn on_meta(&mut self, meta: &str);
+    fn on_value(&mut self, name: &str, value: Value<'_>);
+}
+
+#[derive(Debug)]
+pub(crate) struct Value<'a>(&'a str);
+
+impl Value<'_> {
+    pub(crate) fn bytes(&self) -> Vec<u8> {
+        (0..self.0.len())
+            .step_by(2)
+            .map(|i| u8::from_str_radix(&self.0[i..i + 2], 16).unwrap())
+            .collect()
+    }
+
+    pub(crate) fn int(&self) -> u64 {
+        self.0.parse::<u64>().unwrap()
+    }
+}
+
+pub(crate) fn process_cavp(filename: impl AsRef<Path>, sink: &mut dyn CavpSink) {
+    let f = File::open(filename).expect("cannot open {filename}");
+    for line in BufReader::new(f).lines() {
+        let line = line.unwrap();
+
+        match line.chars().next() {
+            Some('[') => {
+                let right = line.rfind(']').expect("missing ] for meta");
+                sink.on_meta(&line[1..right]);
+                continue;
+            }
+            Some('#') => {
+                println!("{line}");
+                continue;
+            }
+            None => continue,
+            _ => {}
+        };
+
+        if let Some(equal_idx) = line.find(" = ") {
+            let name = &line[..equal_idx];
+            let value = &line[equal_idx + 3..];
+
+            sink.on_value(name, Value(value));
+        } else {
+            println!("ignored {line}");
+        }
+    }
+}
