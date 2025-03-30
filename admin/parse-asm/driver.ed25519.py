ad41eb0386496c7b63e98cec4b29099314abce34
Philip Kannegaard Hayes <philiphayes9@gmail.com>
2025-03-30 16:49:41 -0700

    ed25519: aarch64: `edwards25519_decode` codegen

--- admin/parse-asm/driver.ed25519.py
+++ admin/parse-asm/driver.ed25519.py
@@ -24,3 +24,21 @@ if __name__ == "__main__":
             rust_decl="fn edwards25519_decode(z: &mut [u64; 8], c: &[u8; 32]) -> bool",
         )
         parse_file(input, d)
+
+    with open(
+        "../../thirdparty/s2n-bignum/arm/curve25519/edwards25519_decode_alt.S"
+    ) as input, open(
+        "../../graviola/src/low/aarch64/edwards25519_decode.rs", "w"
+    ) as output:
+        d = RustDriver(output, Architecture_aarch64)
+        d.emit_rust_function(
+            "edwards25519_decode_alt",
+            parameter_map=[
+                ("inout", "z.as_mut_ptr() => ret"),
+                ("inout", "c.as_ptr() => _"),
+            ],
+            return_value=("u64", "ret", "ret == 0"),
+            hoist=["proc", "edwards25519_decode_alt_loop", "ret"],
+            rust_decl="fn edwards25519_decode(z: &mut [u64; 8], c: &[u8; 32]) -> bool",
+        )
+        parse_file(input, d)
