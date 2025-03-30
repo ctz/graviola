64d1ff95d164af1824987ad17110988c90fc73ee
Philip Kannegaard Hayes <philiphayes9@gmail.com>
2025-03-30 16:49:41 -0700

    ed25519: aarch64: `edwards25519_decode` codegen

--- graviola/src/low/aarch64/mod.rs
+++ graviola/src/low/aarch64/mod.rs
@@ -54,6 +54,8 @@ pub(crate) mod bignum_tomont_p384;
 pub(crate) mod cpu;
 pub(crate) mod curve25519_x25519;
 pub(crate) mod curve25519_x25519base;
+#[allow(dead_code)] // TODO(phlip9): remove
+pub(crate) mod edwards25519_decode;
 pub(crate) mod ghash;
 pub(crate) mod p256_montjadd;
 pub(crate) mod p256_montjdouble;
