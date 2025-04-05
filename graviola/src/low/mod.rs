322086f3f05318377e675f343c2ced322763bd70
Philip Kannegaard Hayes <philiphayes9@gmail.com>
2025-04-05 15:01:40 -0700

    ed25519: x86_64: `edwards25519_scalarmuldouble` codegen

--- graviola/src/low/mod.rs
+++ graviola/src/low/mod.rs
@@ -100,6 +100,8 @@ cfg_if::cfg_if! {
         pub(crate) use x86_64::edwards25519_decode::edwards25519_decode;
         #[allow(unused_imports)] // TODO(phlip9): remove
         pub(crate) use x86_64::edwards25519_scalarmulbase::edwards25519_scalarmulbase;
+        #[allow(unused_imports)] // TODO(phlip9): remove
+        pub(crate) use x86_64::edwards25519_scalarmuldouble::edwards25519_scalarmuldouble;
         pub(crate) use x86_64::ghash;
         pub(crate) use x86_64::p256_montjadd::p256_montjadd;
         pub(crate) use x86_64::p256_montjdouble::p256_montjdouble;
