b646b290b7f1534bf6655f1d817a243939a11f87
Joe Birr-Pixton <jpixton@gmail.com>
2025-11-05 08:01:24 +0000

    Enable ed25519 in pairwise TLS tests

--- rustls-graviola/tests/pair.rs
+++ rustls-graviola/tests/pair.rs
@@ -200,6 +200,7 @@ enum KeyType {
     Rsa4096,
     EcdsaP256,
     EcdsaP384,
+    Ed25519,
 }
 
 impl KeyType {
@@ -209,6 +210,7 @@ impl KeyType {
         Self::Rsa4096,
         Self::EcdsaP256,
         Self::EcdsaP384,
+        Self::Ed25519,
     ];
     const RSA: &[Self] = &[Self::Rsa2048, Self::Rsa3072, Self::Rsa4096];
     const ECDSA: &[Self] = &[Self::EcdsaP256, Self::EcdsaP384];
@@ -220,6 +222,7 @@ impl KeyType {
             Self::Rsa4096 => "rsa-4096",
             Self::EcdsaP256 => "ecdsa-p256",
             Self::EcdsaP384 => "ecdsa-p384",
+            Self::Ed25519 => "ed25519",
         }
     }
 
