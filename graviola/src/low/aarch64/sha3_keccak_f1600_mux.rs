f4398d58128164322f9ca763e9475f4d3b4310f0
Brian Pane <brianp+github@brianp.net>
2026-05-30 11:26:56 +0100

    Add a safety comment to the aarch64 sha3 multiplexer

--- graviola/src/low/aarch64/sha3_keccak_f1600_mux.rs
+++ graviola/src/low/aarch64/sha3_keccak_f1600_mux.rs
@@ -4,6 +4,7 @@
 #[inline]
 pub(crate) fn sha3_keccak_f1600(a: &mut [u64; 25], rc: &[u64; 24]) {
     match super::cpu::HaveSha3::check() {
+        // SAFETY: This branch is only called if the CPU has the SHA3 feature.
         Some(_) => unsafe { super::sha3_keccak_f1600_alt::sha3_keccak_f1600(a, rc) },
         None => super::sha3_keccak_f1600::sha3_keccak_f1600(a, rc),
     }
